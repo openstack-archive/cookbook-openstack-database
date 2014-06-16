b2646bb1a950acc9a87b76cacc09c562de96d85a
Ionuț Arțăriși <iartarisi@suse.cz>
2014-06-16 18:49:28 +0200

    initial basic setup
    This first commit will install all the services and has full test
    coverage. Everything is SUSE-specific for now, but the attributes were
    setup so that other platforms can be easily added.
    
    Change-Id: I33be4d259130f479a558d365d8abbbe78df17f44

---
+++ spec/identity_registration-suse_spec.rb
@@ -0,0 +1,76 @@
+# encoding: UTF-8
+
+require_relative 'spec_helper'
+
+describe 'openstack-database::identity_registration' do
+  describe 'suse' do
+    let(:runner) { ChefSpec::Runner.new(SUSE_OPTS) }
+    let(:node) { runner.node }
+    let(:chef_run) { runner.converge(described_recipe) }
+
+    include_context 'database-stubs'
+
+    it 'registers service tenant' do
+      expect(chef_run).to create_tenant_openstack_identity_register(
+        'Register Service Tenant'
+      ).with(
+        auth_uri: 'http://127.0.0.1:35357/v2.0',
+        bootstrap_token: 'bootstrap-token',
+        tenant_name: 'service',
+        tenant_description: 'Service Tenant'
+      )
+    end
+
+    it 'registers service user' do
+      expect(chef_run).to create_user_openstack_identity_register(
+        'Register Service User'
+      ).with(
+        auth_uri: 'http://127.0.0.1:35357/v2.0',
+        bootstrap_token: 'bootstrap-token',
+        tenant_name: 'service',
+        user_name: 'trove',
+        user_pass: 'service-pass'
+      )
+    end
+
+    it 'grants admin role to service user for service tenant' do
+      expect(chef_run).to grant_role_openstack_identity_register(
+        "Grant 'admin' Role to Service User for Service Tenant"
+      ).with(
+        auth_uri: 'http://127.0.0.1:35357/v2.0',
+        bootstrap_token: 'bootstrap-token',
+        tenant_name: 'service',
+        user_name: 'trove',
+        role_name: 'admin'
+      )
+    end
+
+    it 'registers database service' do
+      expect(chef_run).to create_service_openstack_identity_register(
+        'Register Database Service'
+      ).with(
+        auth_uri: 'http://127.0.0.1:35357/v2.0',
+        bootstrap_token: 'bootstrap-token',
+        service_name: 'trove',
+        service_type: 'database',
+        service_description: 'Trove Service'
+      )
+    end
+
+    context 'registers database endpoint' do
+      it 'with default values' do
+        expect(chef_run).to create_endpoint_openstack_identity_register(
+          'Register Database Endpoint'
+        ).with(
+          auth_uri: 'http://127.0.0.1:35357/v2.0',
+          bootstrap_token: 'bootstrap-token',
+          service_type: 'database',
+          endpoint_region: 'RegionOne',
+          endpoint_adminurl: 'http://127.0.0.1:8779/v1.0',
+          endpoint_internalurl: 'http://127.0.0.1:8779/v1.0',
+          endpoint_publicurl: 'http://127.0.0.1:8779/v1.0'
+        )
+      end
+    end
+  end
+end
