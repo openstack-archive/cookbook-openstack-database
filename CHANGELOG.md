5d5d628da9023887cb0eaa3acdd5ff8e46623e99
Mark Vanderwiel <vanderwl@us.ibm.com>
2014-07-29 14:39:12 -0500

    Setting up repo for master Juno Branch
    * Update metadata version
    * Removing Strainer file
    * Adding Gemfile.lock to .gitignore
    * Updating Gems
      * chef (11.12.0)
      * chefspec (4.0.0)
      * rspec (3.0.0)
    
    Change-Id: I054da626e6aa49f70e1c00e49454aa53f541b813
    Partial-Bug: 1349865

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,9 @@
 
 This file is used to list changes made in each version of the cookbook-openstack-database.
 
+## 10.0.0
+* Upgrading to Juno
+
 ## 9.0.1
 * Allow setting passwords via attributes by using the get_password method
 * bump berkshelf to 2.0.18 to allow Supermarket support
--- metadata.rb
+++ metadata.rb
@@ -4,13 +4,13 @@ maintainer_email 'crowbar@dell.com'
 license          'Apache 2.0'
 description      'Installs/Configures trove'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '9.0.1'
+version          '10.0.0'
 recipe           'openstack-database::identity_registration', 'Registers Trove endpoints and service with Keystone'
 recipe           'openstack-database::api', 'Installs API service'
 recipe           'openstack-database::conductor', 'Installs Conductor service'
 recipe           'openstack-database::taskmanager', 'Installs TaskManager service'
 
-depends          'openstack-common', '~> 9.6'
-depends          'openstack-identity', '~> 9.0'
+depends          'openstack-common', '~> 10.0'
+depends          'openstack-identity', '~> 10.0'
 
 supports 'suse'
