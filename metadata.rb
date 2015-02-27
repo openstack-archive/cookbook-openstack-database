0619ecb277693307c3e5251de119670585b745e5
Mark Vanderwiel <vanderwl@us.ibm.com>
2015-02-27 15:09:48 -0600

    Initial kilo updates
    Change-Id: Iae9c37c7e36a9caceaba7d047d2288d1c323fe3f
    Partial-Bug: #1426424

--- metadata.rb
+++ metadata.rb
@@ -4,13 +4,13 @@ maintainer_email 'opscode-chef-openstack@googlegroups.com'
 license          'Apache 2.0'
 description      'Installs/Configures trove'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '10.0.0'
+version          '11.0.0'
 recipe           'openstack-database::identity_registration', 'Registers Trove endpoints and service with Keystone'
 recipe           'openstack-database::api', 'Installs API service'
 recipe           'openstack-database::conductor', 'Installs Conductor service'
 recipe           'openstack-database::taskmanager', 'Installs TaskManager service'
 
-depends          'openstack-common', '>= 10.2.0'
-depends          'openstack-identity', '>= 10.0.0'
+depends          'openstack-common', '>= 11.0.0'
+depends          'openstack-identity', '>= 11.0.0'
 
 supports 'suse'
