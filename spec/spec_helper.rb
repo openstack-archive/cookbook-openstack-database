966daa4abc480143268007eb578263003841d6cc
Samuel Cassiba <samuel.cassiba@workday.com>
2016-07-06 11:52:39 -0700

    Style and lint fixes to support newer ChefDK
    Change-Id: I0f9d09d7cf465ad2c683fe7fc407dff2a8298800

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,17 +9,17 @@ SUSE_OPTS = {
   platform: 'suse',
   version: '11.3',
   log_level: ::LOG_LEVEL
-}
+}.freeze
 REDHAT_OPTS = {
   platform: 'redhat',
   version: '7.1',
   log_level: ::LOG_LEVEL
-}
+}.freeze
 UBUNTU_OPTS = {
   platform: 'ubuntu',
   version: '14.04',
   log_level: ::LOG_LEVEL
-}
+}.freeze
 
 shared_context 'database-stubs' do
   before do
